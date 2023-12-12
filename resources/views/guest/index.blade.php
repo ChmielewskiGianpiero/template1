6f59690a4fbe26ff66c2765c64fea45869d6b5ae
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-12-12 22:03:05 +0100

    first commit

---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">homepage</h1>
+        </div>
+    </div>
+@endsection
